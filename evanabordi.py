4e0947369df6e7cc613f9d7dd26c96540f15b720
Thanabordi Jianrungsang <sunsk128@gmail.com>
2017-01-15 19:04:16 -0800

    WIP

--- evanabordi.py
+++ evanabordi.py
@@ -17,16 +17,28 @@ with open('input/biology.csv') as csv_file:
     cv_t = CountVectorizer(tokenizer=nltk.word_tokenize)
     t = cv_t.fit_transform(tags_training).T
 
-    uki1 = t.dot(w)
-    uki3 = uki1.sum(axis = 1)
     epsilon = 1
-    n = uki1.shape[1]
-    gnb = np.log((uki1.toarray() + epsilon) / (uki3 + n * epsilon))
+
+    absolute_word_prob = w.sum(axis = 0).A[0].astype(np.float) + epsilon
+    absolute_word_prob /= sum(absolute_word_prob)
+    np.log(absolute_word_prob, out = absolute_word_prob)
+
+    uki1 = t.dot(w)
+    uki3 = uki1.sum(axis = 1).A[:, 0].astype(np.float)
+    n = uki1.shape[0]
+    gnb = np.log((uki1.A.astype(np.float) + epsilon) / (uki3[:, None] + n * epsilon))
 
     num_docs = len(bodies_training)
-    prior = np.log(t.sum(axis=1)/num_docs)
+    prior = np.log(t.sum(axis=1).A[:, 0].astype(np.float)/num_docs)
 
-    x = cv_w.transform([bodies_validating[0]])
-    gnb.dot(x)
+    x = cv_w.transform([bodies_validating[0]]).astype(np.float).toarray()[0]
+    likelihood = gnb.dot(x)
+    absolute_prob = absolute_word_prob.dot(x)
+
+    posterior = prior + likelihood - absolute_prob
+    print([k for k, v in cv_t.vocabulary_.items() if posterior[v] > np.log(0.5)])
+    #print(cv_t.inverse_transform(posterior > np.log(0.5)))
+    print(tags_validating[0])
+
 
     pdb.set_trace()
